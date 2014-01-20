b794008107620030e058903178ceb8c0d8ee166f
joel <jmohler@gamry.com>
2014-01-20 11:22:53 -0500

    refactor python helpers as real files in repo

--- build_dep.cmd
+++ build_dep.cmd
@@ -13,56 +13,31 @@ rd /S /Q %BUILD%
 mkdir %INCLIB%
 mkdir %BUILD%
 
-rem Helper Scripts
-echo import sys, zipfile > unzip.py
-echo with zipfile.ZipFile(sys.argv[1]) as zf: >> unzip.py
-echo     zf.extractall(sys.argv[2]) >> unzip.py
-
-echo import sys, tarfile > untar.py
-echo with tarfile.open(sys.argv[1], 'r:gz') as tgz: >> untar.py
-echo     tgz.extractall(sys.argv[2]) >> untar.py
-
-echo import sys, os > fixproj.py
-echo with open(sys.argv[1], 'r') as fd: >> fixproj.py
-echo     content = '\n'.join(line.strip() for line in fd if line.strip()) >> fixproj.py
-echo if len(sys.argv) == 3: >> fixproj.py
-echo     content = content.replace('Win32', sys.argv[2]).replace('x64', sys.argv[2]) >> fixproj.py
-echo with open(sys.argv[1], 'w') as fd: >> fixproj.py
-echo     fd.write(content) >> fixproj.py
-
-echo import sys, os, urllib.parse, urllib.request > fetch.py
-echo name = urllib.parse.urlsplit(sys.argv[1])[2].split('/')[-1] >> fetch.py
-echo if not os.path.exists(name): >> fetch.py
-echo     print("Fetching", sys.argv[1]) >> fetch.py
-echo     content = urllib.request.urlopen(sys.argv[1]).read() >> fetch.py
-echo     with open(name, 'wb') as fd: >> fetch.py
-echo         fd.write(content) >> fetch.py
-
 rem Get freetype
-py -3 fetch.py http://download.savannah.gnu.org/releases/freetype/ft2411.zip
-py -3 unzip.py ft2411.zip %BUILD%
+py -3 helpers\fetch.py http://download.savannah.gnu.org/releases/freetype/ft2411.zip
+py -3 helpers\unzip.py ft2411.zip %BUILD%
 set FREETYPE=%BUILD%\freetype-2.4.11
 copy /Y /B ft2411.zip %INCLIB%
 
 rem Get zlib
-py -3 fetch.py http://zlib.net/zlib128.zip
-py -3 unzip.py zlib128.zip %BUILD%
+py -3 helpers\fetch.py http://zlib.net/zlib128.zip
+py -3 helpers\unzip.py zlib128.zip %BUILD%
 set ZLIB=%BUILD%\zlib-1.2.8
 copy /Y /B zlib128.zip %INCLIB%
 
 rem Get libpng
-rem py -3 fetch.py ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.6.7.tar.gz
-py -3 fetch.py http://hivelocity.dl.sourceforge.net/project/libpng/libpng16/1.6.7/libpng-1.6.7.tar.gz
-py -3 untar.py libpng-1.6.7.tar.gz %BUILD%
+rem py -3 helpers\fetch.py ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.6.7.tar.gz
+py -3 helpers\fetch.py http://hivelocity.dl.sourceforge.net/project/libpng/libpng16/1.6.7/libpng-1.6.7.tar.gz
+py -3 helpers\untar.py libpng-1.6.7.tar.gz %BUILD%
 set LIBPNG=%BUILD%\libpng-1.6.7
 copy /Y /B libpng-1.6.7.tar.gz %INCLIB%
 
 rem Get tcl/tk
-py -3 fetch.py http://hivelocity.dl.sourceforge.net/project/tcl/Tcl/8.5.13/tcl8513-src.zip
-py -3 unzip.py tcl8513-src.zip %BUILD%
+py -3 helpers\fetch.py http://hivelocity.dl.sourceforge.net/project/tcl/Tcl/8.5.13/tcl8513-src.zip
+py -3 helpers\unzip.py tcl8513-src.zip %BUILD%
 copy /Y /B tcl8513-src.zip %INCLIB%
-py -3 fetch.py http://hivelocity.dl.sourceforge.net/project/tcl/Tcl/8.5.13/tk8513-src.zip
-py -3 unzip.py tk8513-src.zip %BUILD%
+py -3 helpers\fetch.py http://hivelocity.dl.sourceforge.net/project/tcl/Tcl/8.5.13/tk8513-src.zip
+py -3 helpers\unzip.py tk8513-src.zip %BUILD%
 copy /Y /B tk8513-src.zip %INCLIB%
 
 mkdir %INCLIB%\tcl85\include\X11
@@ -110,8 +85,8 @@ endlocal
 
 rem Build freetype
 setlocal
-py -3 %~dp0\fixproj.py %FREETYPE%\builds\win32\vc2008\freetype.sln x64
-py -3 %~dp0\fixproj.py %FREETYPE%\builds\win32\vc2008\freetype.vcproj x64
+py -3 %~dp0\helpers\fixproj.py %FREETYPE%\builds\win32\vc2008\freetype.sln x64
+py -3 %~dp0\helpers\fixproj.py %FREETYPE%\builds\win32\vc2008\freetype.vcproj x64
 rd /S /Q %FREETYPE%\objs
 %MSBUILD% %FREETYPE%\builds\win32\vc2008\freetype.sln /t:Clean;Build /p:Configuration="LIB Release";Platform=x64
 xcopy /E /Q %FREETYPE%\include %INCLIB%
@@ -161,8 +136,8 @@ endlocal
 
 rem Build freetype
 setlocal
-%~dp0\fixproj.py %FREETYPE%\builds\win32\vc2008\freetype.sln Win32
-%~dp0\fixproj.py %FREETYPE%\builds\win32\vc2008\freetype.vcproj Win32
+%~dp0\helpers\fixproj.py %FREETYPE%\builds\win32\vc2008\freetype.sln Win32
+%~dp0\helpers\fixproj.py %FREETYPE%\builds\win32\vc2008\freetype.vcproj Win32
 rd /S /Q %FREETYPE%\objs
 %MSBUILD% %FREETYPE%\builds\win32\vc2008\freetype.sln /t:Clean;Build /p:Configuration="LIB Release";Platform=Win32
 xcopy /E /Q %FREETYPE%\include %INCLIB%
@@ -212,8 +187,8 @@ endlocal
 
 rem Build freetype
 setlocal
-py -3 %~dp0\fixproj.py %FREETYPE%\builds\win32\vc2010\freetype.sln x64
-py -3 %~dp0\fixproj.py %FREETYPE%\builds\win32\vc2010\freetype.vcxproj x64
+py -3 %~dp0\helpers\fixproj.py %FREETYPE%\builds\win32\vc2010\freetype.sln x64
+py -3 %~dp0\helpers\fixproj.py %FREETYPE%\builds\win32\vc2010\freetype.vcxproj x64
 rd /S /Q %FREETYPE%\objs
 %MSBUILD% %FREETYPE%\builds\win32\vc2010\freetype.sln /t:Clean;Build /p:Configuration="Release";Platform=x64
 xcopy /E /Q %FREETYPE%\include %INCLIB%
@@ -263,8 +238,8 @@ endlocal
 
 rem Build freetype
 setlocal
-%~dp0\fixproj.py %FREETYPE%\builds\win32\vc2010\freetype.sln Win32
-%~dp0\fixproj.py %FREETYPE%\builds\win32\vc2010\freetype.vcxproj Win32
+%~dp0\helpers\fixproj.py %FREETYPE%\builds\win32\vc2010\freetype.sln Win32
+%~dp0\helpers\fixproj.py %FREETYPE%\builds\win32\vc2010\freetype.vcxproj Win32
 rd /S /Q %FREETYPE%\objs
 %MSBUILD% %FREETYPE%\builds\win32\vc2010\freetype.sln /t:Clean;Build /p:Configuration="Release";Platform=Win32
 xcopy /E /Q %FREETYPE%\include %INCLIB%
---
+++ helpers/untar.py
@@ -0,0 +1,3 @@
+import sys, tarfile 
+with tarfile.open(sys.argv[1], 'r:gz') as tgz: 
+    tgz.extractall(sys.argv[2]) 
