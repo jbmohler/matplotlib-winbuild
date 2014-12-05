c155ed13567034bf9295474fc7d8b31c8d22cf1b
Andrew Collette <andrew.collette@gmail.com>
2014-12-05 12:56:27 -0700

    Add option to skip building Tcl/Tk

--- buildall.py
+++ buildall.py
@@ -6,7 +6,8 @@ import optparse
 
 
 def main(options):
-    utils.build_tcl()
+    if not options.no_tcl:
+        utils.build_tcl()
     utils.build_zlib()
     utils.build_libpng()
     utils.build_freetype()
@@ -43,6 +44,8 @@ if __name__ == '__main__':
                                           default=default_path, help="Matplotlib repo directory")
     parser.add_option('--setup_cmd', '-s', action="store", dest="setup_cmd",
                                            default="install", help="Command to pass to setup.py")
+    parser.add_option('--no_tcl', '-n', action="store_true", dest="no_tcl",
+                                          default=False, help="Don't build Tcl/Tk (for headless MPL servers)")
     options, rem = parser.parse_args()
 
     main(options)
